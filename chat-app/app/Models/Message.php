3d280aef9ed45979a7db1f3896fc5b3973b4c578
luis143d <108910590+luis143d@users.noreply.github.com>
2025-04-29 12:42:18 -0500

    Add chat-app/app/Models/Message.php 1745948536953

---
+++ chat-app/app/Models/Message.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'message',
+    ];
+
+    /**
+     * Get the user that owns the message.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
